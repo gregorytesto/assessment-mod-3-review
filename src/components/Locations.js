e7b1ff9557e28b8820866d80544548248d99227f
Greg Testo <gregt@pursuit.org>
2021-12-17 16:50:05 -0400

    passed location page tests

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,9 +1,44 @@
 import { Component  } from "react";
 
 class Locations extends Component{
+    constructor(){
+        super();
+        this.state = {
+            locations: [],
+            displayLocations: false
+        }
+    }
+
+    componentDidMount(){
+        fetch("https://ghibliapi.herokuapp.com/locations")
+            .then(res=>res.json())
+            .then(data=>{
+                this.setState({
+                    locations: data
+                })
+            })
+    }
+    handleToggleLocations=()=>{
+        this.setState({
+            displayLocations: !this.state.displayLocations
+        })
+    }
+
     render(){
+        let locationsElArr = this.state.locations.map((location)=>{
+            return <li>{location.name}</li>
+        })
+
         return(
-            <h1>Locations Page</h1>
+            <div className="locations">
+                <h1>List of Locations</h1>
+                <button onClick={this.handleToggleLocations}>{this.state.displayLocations ? "Hide": "Show"} Locations</button>
+                { this.state.displayLocations &&
+                    <ul>
+                        { locationsElArr }
+                    </ul>
+                }
+            </div>
         )
     }
 }
