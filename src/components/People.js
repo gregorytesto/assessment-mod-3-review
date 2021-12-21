cf0b22d277c2810ab918ce93cc10b7d54641f6ce
Greg Testo <gregt@pursuit.org>
2021-12-21 12:00:34 -0400

    people page in progress

--- src/components/People.js
+++ src/components/People.js
@@ -1,11 +1,39 @@
-import { Component  } from "react";
+import { useEffect, useState } from 'react';
 
-class People extends Component{
-    render(){
-        return(
-            <h1>People Page</h1>
-        )
+const People=()=>{
+    const [ people, setPeople ] = useState([]);
+    const [ personInput, setPersonInput ] = useState("");
+
+    useEffect(()=>{
+        fetch("https://ghibliapi.herokuapp.com/people")
+            .then((res)=>res.json())
+            .then((data)=>{
+                setPeople(data);
+            })
+    }, []);
+
+    const handlePersonInput=(e)=>{
+        setPersonInput(e.target.value);
     }
+
+    const handleSubmit=()=>{
+        console.log("People: ", people);
+        console.log("Input: ", personInput);
+    }
+
+    return(
+        <div>
+            <div>People Page</div>
+            <input 
+                placeholder="Find Your Person"
+                type="text"
+                value={ personInput }
+                onChange={ handlePersonInput }
+            />
+            <button onClick={ handleSubmit }>Submit</button>
+            
+        </div>
+    )
 }
 
 export default People;
