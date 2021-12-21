487913310517d8ba6de35f1f867d8d266b82af23
Greg Testo <gregt@pursuit.org>
2021-12-21 12:55:35 -0400

    finished assessment with hooks

--- src/components/People.js
+++ src/components/People.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 const People=()=>{
     const [ people, setPeople ] = useState([]);
     const [ personInput, setPersonInput ] = useState("");
+    const [ searchResult, setSearchResult ] = useState(null);
 
     useEffect(()=>{
         fetch("https://ghibliapi.herokuapp.com/people")
@@ -17,13 +18,28 @@ const People=()=>{
     }
 
     const handleSubmit=()=>{
-        console.log("People: ", people);
-        console.log("Input: ", personInput);
+        let result = people.find(person=>{
+            return person.name.toLowerCase() ===  personInput.toLowerCase();
+        })
+        setSearchResult(result);
     }
 
+    // let resultToRender;
+    // if(searchResult){
+    //     resultToRender = (
+    //         <div>
+    //             <h3>Name: { searchResult.name }</h3>
+    //             <h3>Age: { searchResult.age }</h3>
+    //             <h3>Gender: { searchResult.gender }</h3>
+    //         </div>
+    //     )
+    // } else if (searchResult === undefined) {
+    //     resultToRender = <h3>Not Found</h3>
+    // }
+
     return(
-        <div>
-            <div>People Page</div>
+        <div className="people">
+            <div>Search for a Person</div>
             <input 
                 placeholder="Find Your Person"
                 type="text"
@@ -31,7 +47,19 @@ const People=()=>{
                 onChange={ handlePersonInput }
             />
             <button onClick={ handleSubmit }>Submit</button>
-            
+
+            {/* { resultToRender } */}
+            { searchResult &&
+                <div>
+                    <h3>Name: { searchResult.name }</h3>
+                    <h3>Age: { searchResult.age }</h3>
+                    <h3>Gender: { searchResult.gender }</h3>
+                </div>
+            }
+            { searchResult === undefined &&
+                <h3>Not Found</h3>
+            }
+
         </div>
     )
 }
